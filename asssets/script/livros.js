70d85d32ce340d3687eff2051c0bdf93c6c49fb6
ANA KAROLYNE DA SILVA <ana.ksilva28@senacsp.edu.br>
2024-09-17 22:32:27 -0300

    adicionando tailwind, criando modal de criar novo livro

--- asssets/script/livros.js
+++ asssets/script/livros.js
@@ -1,10 +1,23 @@
+let btnCloseCreateBook = document.querySelector('#btn-close-create-book')
+let btnOpenCreateBook = document.querySelector('#btn-open-modal-create-book')
+
+btnCloseCreateBook.addEventListener('click', () => {
+    document.querySelector('#div-create-book').setAttribute('style','display: none')
+})
+btnOpenCreateBook.addEventListener('click', () => {
+    document.querySelector('#div-create-book').setAttribute('style','display: flex')
+})
+
+
 window.onload = function() {
     let tbody = document.querySelector('#table-book tbody')
 
     for(let i = 0; i < 9; i++){
         var tr = document.createElement('tr');
+        tr.setAttribute('class', ' border-b bg-gray-800 border-gray-700')
 
         var th = document.createElement('th');
+        th.setAttribute('class', 'px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white')
         th.setAttribute('scope', 'row');
         th.textContent = '1'; // Conteúdo da célula
         tr.appendChild(th);
@@ -35,8 +48,8 @@ window.onload = function() {
         var editar = document.createElement('button');
         var excluir = document.createElement('button');
     
-        editar.classList.add('btn-class-action')
-        excluir.classList.add('btn-class-action')
+        editar.setAttribute('class', 'bg-white text-black px-3 py-2 rounded mx-1')
+        excluir.setAttribute('class', 'bg-white text-black px-3 py-2 rounded mx-1')
         editar.textContent = "editar"
         excluir.textContent = "excluir"
     
